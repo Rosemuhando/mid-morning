85b56ba85b9a7c53b8ee2ca214faba139752b62b
rosemuhando <rosemuhando90@gmail.com>
2025-02-03 13:26:25 +0300

    this is my fourth commit

---
+++ Variables.py
@@ -0,0 +1,21 @@
+#Variable is a container used to store a value
+
+number = 10
+firstname = "rose"
+lastname = "otieno"
+language ="python"
+a =10
+b =15
+
+
+
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname,number)
+print(language,"is a web programming language")
+print(a + b)
+#the sum of 10 and 15 is 25
+
+print("the sum of a and b is 25")
